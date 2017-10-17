0d927eacbf2c6e0b8cb37fa09080534b2d1455fc
Taylor <tbuchhei@gmail.com>
2017-10-17 15:26:17 -0700

    Creating a replay system

---
+++ Twin Stix 2.5/Assets/Player/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CrossPlatformInputManager.GetAxis("Vertical") > 0)
+        {
+            print("w pressed");
+        }
+        if (CrossPlatformInputManager.GetAxis("Vertical") < 0)
+        {
+            print("s pressed");
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") > 0)
+        {
+            print("d pressed");
+        }
+        if (CrossPlatformInputManager.GetAxis("Horizontal") < 0)
+        {
+            print("a pressed");
+        }
+    }
+}
---
+++ Twin Stix 2.5/Assets/ReplaySystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaySystem : MonoBehaviour {
+
+    private const int bufferFrames = 1000;
+    private MyKeyFrame[] keyFrames = new MyKeyFrame[bufferFrames];
+    private Rigidbody rigidBody;
+    private GameManager manager;
+    private int lastFrameRecorded = 0;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        manager = GameObject.FindObjectOfType<GameManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(manager.recording == true)
+        {
+            Record();
+        }
+        else
+        {
+            PlayBack();
+        }
+    }
+
+    public void PlayBack()
+    {
+        rigidBody.isKinematic = true;
+        int frame = Time.frameCount % lastFrameRecorded;
+        print("Reading frame " + frame);
+        transform.position = keyFrames[frame].position;
+        transform.rotation = keyFrames[frame].rotation;
+    }
+
+    public void Record()
+    {
+        lastFrameRecorded++;
+        rigidBody.isKinematic = false;
+        int frame = Time.frameCount % bufferFrames;
+        float time = Time.time;
+        print("Writing frame " + frame);
+        keyFrames[frame] = new MyKeyFrame(time, transform.position, transform.rotation);
+    }
+}
+
+/// <summary>
+/// A structure for storing, Time, Position, and Rotation
+/// </summary>
+public struct MyKeyFrame
+{
+    public float frameTime;
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public MyKeyFrame(float time, Vector3 position, Quaternion rotation)
+    {
+        this.frameTime = time;
+        this.position = position;
+        this.rotation = rotation;
+    }
+}
