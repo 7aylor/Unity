60b8b258991c87e3777a1fe9560cecce147875f8
Taylor <tbuchhei@gmail.com>
2017-10-18 22:59:42 -0700

    Finished Twin Stix tutorial. Camera rotates around plane.

---
+++ Twin Stix 2.5/Assets/Camera/CameraPan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPan : MonoBehaviour {
+
+    private Transform player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+
+    private void LateUpdate()
+    {
+        transform.LookAt(player);
+    }
+}
---
+++ Twin Stix 2.5/Assets/Camera/SelfieStick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieStick : MonoBehaviour {
+
+    public float panSpeed = 2f;
+
+    private Transform player;
+    private Vector3 armRotation;    
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        armRotation = transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        armRotation.y += Input.GetAxis("RHoriz") * panSpeed;
+        armRotation.x += Input.GetAxis("RVert") * panSpeed;
+        transform.position = player.position;
+        transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
