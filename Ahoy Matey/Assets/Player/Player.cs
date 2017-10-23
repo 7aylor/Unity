6ee7eb91dee2ba5a7780e2733db335c923157173
Taylor <tbuchhei@gmail.com>
2017-10-23 16:39:09 -0700

    Finished Ahoy Matey Tutorials. Client players can control their players and their movement is reflected on on clients.

---
+++ Ahoy Matey/Assets/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    private Vector3 inputValue;
+    private Camera camera;
+
+    // Update is called once per frame
+    void Update () {
+        if (isLocalPlayer)
+        {
+            inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+            inputValue.y = 0f;
+            inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+            transform.Translate(inputValue);
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+
+        camera = GetComponentInChildren<Camera>();
+
+        if (camera != null)
+        {
+            camera.enabled = true;
+        }
+    }
+}
