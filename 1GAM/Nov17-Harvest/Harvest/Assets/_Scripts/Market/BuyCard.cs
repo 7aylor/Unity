b56daf4ce7622e969a68259ed02b72e0e9e1be13
Taylor <tbuchhei@gmail.com>
2017-11-08 23:01:19 -0800

    Started Coding the Market functionaltiy.

---
+++ 1GAM/Nov17-Harvest/Harvest/Assets/_Scripts/Market/BuyCard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyCard : MonoBehaviour
+{
+    private GameObject child;
+    private Market market;
+
+    void Start()
+    {
+        child = transform.GetChild(0).gameObject;
+        market = FindObjectOfType<Market>();
+    }
+
+    public void Buy()
+    {
+        //check for adequate gold
+
+        //check for an open slot in the respective hand
+
+        //delete card from market
+
+        if(gameObject.tag != "Market_Deck")
+        {
+            Destroy(child);
+        }
+
+        //move other cards to new positions
+        market.ShiftCardsForwad();
+
+        //draw new card from deck
+    }
+
+
+}
---
+++ 1GAM/Nov17-Harvest/Harvest/Assets/_Scripts/Market/Market.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Market : MonoBehaviour {
+
+    public GameObject[] cards;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    /// <summary>
+    /// Gets a random card to deal
+    /// </summary>
+    /// <returns></returns>
+    public GameObject DealRandomCard()
+    {
+        return cards[Random.Range(0, cards.Length - 1)];
+    }
+
+    /// <summary>
+    /// Shifts the cards up dependings which card was just removed
+    /// </summary>
+    public void ShiftCardsForwad()
+    {
+        int counter = 0;
+        int shiftIndex = 0;
+
+        //finds the index of the card that was just removed
+        foreach(Transform child in transform)
+        {
+            if(child.childCount == 0)
+            {
+                shiftIndex = counter;
+                break;
+            }
+            counter++;
+        }
+
+        //deals with the cards of the card being in the free spot
+        if(shiftIndex == 0)
+        {
+            Instantiate(transform.GetChild(1).GetChild(0), transform.GetChild(0));
+            //Destroy(transform.GetChild(1));
+            //Instantiate(transform.GetChild(2).GetChild(0), transform.GetChild(1));
+            //Destroy(transform.GetChild(2));
+            //Instantiate(DealRandomCard(), transform.GetChild(2));
+
+            //transform.
+        }
+    }
+
+}
