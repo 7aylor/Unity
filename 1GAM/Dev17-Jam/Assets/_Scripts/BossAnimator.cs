d2ff64e33d462d76d67dd297c5b9901a433be2a4
Taylor <tbuchhei@gmail.com>
2017-12-15 22:11:50 -0800

    Added boss to the game

---
+++ 1GAM/Dev17-Jam/Assets/_Scripts/BossAnimator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class BossAnimator : MonoBehaviour {
+
+    private enum Direction { Right, Left}
+    private Direction direction;
+    private Animator animator;
+    private float scaleX;
+    private bool flipDirection = false;
+
+
+	// Use this for initialization
+	void Start () {
+        direction = GetRandomDirection();
+        animator = GetComponent<Animator>();
+        scaleX = transform.localScale.x;
+	}
+
+    private void Update()
+    {
+        HandleWalk();
+    }
+
+    private Direction GetRandomDirection()
+    {
+        int randDir = Random.Range(0, 2);
+        return (Direction)randDir;
+    }
+
+    private void HandleWalk()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walking"))
+        {
+            if (direction == Direction.Right)
+            {
+                transform.Translate(Vector3.right * Time.deltaTime);
+
+                if(flipDirection == false)
+                {
+                    transform.localScale = new Vector3(scaleX, transform.localScale.y, transform.localScale.z);
+                    direction = Direction.Left;
+                    flipDirection = true;
+                }
+            }
+            else if((direction == Direction.Left))
+            {
+                transform.Translate(Vector3.left * Time.deltaTime);
+                if(flipDirection == false)
+                {
+                    transform.localScale = new Vector3(-scaleX, transform.localScale.y, transform.localScale.z);
+                    direction = Direction.Right;
+                    flipDirection = true;
+                }
+            }
+        }
+        else
+        {
+            flipDirection = false;
+        }
+    }
+}
