52ce5fd51ecfeb9f891838ba20f49f4baab77b3c
Taylor <tbuchhei@gmail.com>
2018-01-04 14:56:55 -0800

    Finished adding all direction run cycles. Added idle animations for horizontal and down

--- 1GAM/Jan18-Rebirth/Assets/_Scripts/Caveman_Move.cs
+++ 1GAM/Jan18-Rebirth/Assets/_Scripts/Caveman_Move.cs
@@ -7,11 +7,13 @@ public class Caveman_Move : MonoBehaviour {
     public float speed = 1;
 
     private bool run_horiz = false;
+    private bool run_down = false;
     private Animator animator;
     private SpriteRenderer sprite;
+    private bool run_up = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
 	}
@@ -32,29 +34,68 @@ public class Caveman_Move : MonoBehaviour {
         {
             animator.SetBool("RunHoriz", false);
         }
+        if (run_down == true)
+        {
+            animator.SetBool("RunDown", true);
+        }
+        else
+        {
+            animator.SetBool("RunDown", false);
+        }
+        if (run_up == true)
+        {
+            animator.SetBool("RunUp", true);
+        }
+        else
+        {
+            animator.SetBool("RunUp", false);
+        }
     }
 
     private void CheckForInput()
     {
-        //left
-        if (Input.GetKey(KeyCode.A))
-        {
-            run_horiz = true;
-            sprite.flipX = true;
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        //right
-        else if (Input.GetKey(KeyCode.D))
-        {
-            run_horiz = true;
-            sprite.flipX = false;
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        if((Input.anyKey == true)){
+            //left
+            if (Input.GetKey(KeyCode.A))
+            {
+                run_horiz = true;
+                run_down = false;
+                run_up = false;
+                sprite.flipX = true;
+                transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+            //right
+            if (Input.GetKey(KeyCode.D))
+            {
+                run_horiz = true;
+                run_down = false;
+                run_up = false;
+                sprite.flipX = false;
+                transform.Translate(Vector3.right * speed * Time.deltaTime);
+            }
+            //down
+            if (Input.GetKey(KeyCode.S))
+            {
+                run_down = true;
+                run_horiz = false;
+                run_up = false;
+                transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+            //up
+            if (Input.GetKey(KeyCode.W))
+            {
+                run_up = true;
+                run_horiz = false;
+                run_down = false;
+                transform.Translate(Vector3.up * speed * Time.deltaTime);
+            }
         }
         else
         {
             run_horiz = false;
+            run_down = false;
+            run_up = false;
         }
-        
     }
 
 
