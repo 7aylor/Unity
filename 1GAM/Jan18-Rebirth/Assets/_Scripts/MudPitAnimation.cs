1c0be81d8081f029e9c627f89704dc48352f8ac6
Taylor <tbuchhei@gmail.com>
2018-01-08 23:30:28 -0800

    Added bubble popping animation and drew the shaman

---
+++ 1GAM/Jan18-Rebirth/Assets/_Scripts/MudPitAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudPitAnimation : MonoBehaviour {
+
+    private Animator animator;
+    private float timeSinceLastPop = 0f;
+    private float timeBetweenPops;
+    private string[] triggerNames = { "PopTop", "PopBot" };
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        GetRandomTimeBetweenPops();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(timeSinceLastPop >= timeBetweenPops)
+        {
+            animator.SetTrigger(GetRandomTrigger());
+            timeSinceLastPop = 0;
+            GetRandomTimeBetweenPops();
+        }
+        else
+        {
+            timeSinceLastPop += Time.deltaTime;
+        }
+	}
+
+    private void GetRandomTimeBetweenPops()
+    {
+        timeBetweenPops = Random.Range(5f, 10f);
+    }
+
+    private string GetRandomTrigger()
+    {
+        return triggerNames[Random.Range(0, triggerNames.Length)];
+    }
+}
