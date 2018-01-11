143268653ff4692ccc9535af58e2656efb3b4678
Taylor <tbuchhei@gmail.com>
2018-01-11 15:01:13 -0800

    Finished throw animations. Started adding into Unity

---
+++ 1GAM/Jan18-Rebirth/Assets/_Scripts/Caveman_Throw.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Caveman_Throw : MonoBehaviour {
+
+    private Animator animator;
+    private AnimatorStateInfo currentAnimation;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        currentAnimation = animator.GetCurrentAnimatorStateInfo(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
