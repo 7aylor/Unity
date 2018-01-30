611775a11a7b7eaa775e3fb71bfcaf4beb635b69
Taylor <tbuchhei@gmail.com>
2018-01-29 17:09:00 -0800

    More tweaking

--- 1GAM/Jan18-Rebirth/Assets/_Scripts/ActivateRunes.cs
+++ 1GAM/Jan18-Rebirth/Assets/_Scripts/ActivateRunes.cs
@@ -7,11 +7,13 @@ public class ActivateRunes : MonoBehaviour {
     private SpriteRenderer[] runes;
     private DialogueWindow dw;
     private int currentRune = 0;
+    private Shaman shaman;
 
 	// Use this for initialization
 	void Start () {
         runes = transform.GetComponentsInChildren<SpriteRenderer>();
         dw = FindObjectOfType<DialogueWindow>();
+        shaman = FindObjectOfType<Shaman>();
 	}
 
     public void EnableRune(bool enable)
@@ -22,12 +24,15 @@ public class ActivateRunes : MonoBehaviour {
             {
                 //if I get to it, do an animation here
                 runes[currentRune].enabled = true;
+                shaman.Summon(true); ///////CHECK ON THIS
+                StartCoroutine("LerpColorAlpha", currentRune);
                 currentRune++;
                 dw.DecreaseWordTracker();
             }
             else
             {
                 Debug.Log("CurrentRune out of bounds");
+                //maybe spawn portal here?
             }
             
         }
@@ -49,6 +54,19 @@ public class ActivateRunes : MonoBehaviour {
     public int GetActiveRuneCount()
     {
         return currentRune;
+    }
+
+    private IEnumerator LerpColorAlpha(int index)
+    {
+
+        Color spriteColor = runes[index].color;
+        for (int i = 1; i <= 100; i++)
+        {
+            spriteColor.a = i / 100f;
+            runes[index].color = spriteColor;
+            yield return new WaitForEndOfFrame();
+        }
+        shaman.Summon(false);
     }
 
 }
---
+++ 1GAM/Jan18-Rebirth/Assets/_Scripts/SpawnPortal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPortal : MonoBehaviour {
+
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	private void EnablePortal()
+    {
+        sprite.enabled = true;
+        StartCoroutine("LerpColorAlpha");
+    }
+
+    private IEnumerator LerpColorAlpha()
+    {
+
+        Color spriteColor = sprite.color;
+        for (int i = 1; i <= 100; i++)
+        {
+            spriteColor.a = i/100f;
+            sprite.color = spriteColor;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+}
