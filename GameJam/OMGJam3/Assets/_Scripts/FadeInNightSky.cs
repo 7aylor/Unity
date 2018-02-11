83e28d33e64c3f29fd0ee4eb2a201fe5b7640beb
Taylor <tbuchhei@gmail.com>
2018-02-10 23:17:51 -0800

    Started getting scene changes set up

---
+++ GameJam/OMGJam3/Assets/_Scripts/FadeInNightSky.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInNightSky : MonoBehaviour {
+
+    Image nightSkyImage;
+
+    private void Awake()
+    {
+        nightSkyImage = GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        //FadeInAlpha();
+    }
+
+    public void FadeInAlpha()
+    {
+        StartCoroutine("LightNightSky");
+    }
+
+    private IEnumerator LightNightSky()
+    {
+        for(int i = 0; i < 100; i++)
+        {
+            Color c = nightSkyImage.color;
+            c.a += 0.01f;
+            nightSkyImage.color = c;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
---
+++ GameJam/OMGJam3/Assets/_Scripts/SceneChange.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneChange : MonoBehaviour {
+
+    public Image clock;
+    public Image picture;
+    public Image window;
+    public Image skyline;
+    public Image skylineNight;
+    public Image snowGlobe;
+    public Image bonzai;
+    public Image cup;
+    public Image ledger;
+
+	// Use this for initialization
+	void Start () {
+        //EarlyGame();
+        //MidGame();
+        EndGame();
+	}
+	
+    public void EarlyGame()
+    {
+        clock.enabled = true;
+        picture.enabled = true;
+        window.enabled = false;
+        skyline.enabled = false;
+        skylineNight.enabled = false;
+        snowGlobe.enabled = false;
+        bonzai.enabled = false;
+        cup.enabled = false;
+        ledger.enabled = false;
+    }
+
+    public void MidGame()
+    {
+        clock.enabled = true;
+        picture.enabled = false;
+        window.enabled = true;
+        skyline.enabled = false;
+        skylineNight.enabled = false;
+        snowGlobe.enabled = true;
+        bonzai.enabled = false;
+        cup.enabled = true;
+        ledger.enabled = true;
+    }
+
+    public void EndGame()
+    {
+        clock.enabled = false;
+        picture.enabled = false;
+        window.enabled = false;
+        skyline.enabled = true;
+        skylineNight.enabled = false;
+        snowGlobe.enabled = true;
+        bonzai.enabled = true;
+        cup.enabled = true;
+        ledger.enabled = true;
+    }
+}
