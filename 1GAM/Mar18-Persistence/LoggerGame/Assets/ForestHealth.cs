8f403392971f0a72eb0a35ee2c73b45835391100
Taylor <tbuchhei@gmail.com>
2018-03-18 21:25:31 -0700

    drew event character

---
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/ForestHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ForestHealth : MonoBehaviour {
+
+    private TMP_Text text;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    void Start () {
+        text.text = GameManager.instance.forestHealth.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/GameManager.cs
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/GameManager.cs
@@ -9,16 +9,26 @@ public class GameManager : MonoBehaviour {
     public int sizeY;
     public bool playerSelected = false;
     public Player selectedPlayer;
+    public int forestHealth;
+    public int dollars;
+    public int lumber;
+    public int demand;
 
     public static GameManager instance;
 
     private void Awake()
     {
         instance = this;
+        forestHealth = 100;
+        dollars = 1000;
+        lumber = 0;
+        demand = 0;
     }
 
     public void InstantiateMap(int x, int y)
     {
         map = new int[x, y];
+        
+
     }
 }
