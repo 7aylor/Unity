9dbb1d8470c0618c39283b5b7e9d336367aed335
Taylor <tbuchhei@gmail.com>
2018-03-20 22:08:22 -0700

    Got an Eagle added and animated

---
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/Bird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour {
+
+    public float speed;
+    public bool isRight = true; //set by the spawner
+    private int direction;
+    private SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+		if(isRight == true)
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = -1;
+            sprite.flipX = true;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector2(direction, Random.Range(-0.25f, 0.25f)) * speed * Time.deltaTime);
+	}
+}
---
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/BirdSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour {
+
+    public bool isRight;
+    public GameObject bird;
+    private float maxTimeBetweenSpawns;
+    private float timeSinceLastSpawn;
+
+	// Use this for initialization
+	void Start () {
+        timeSinceLastSpawn = 0;
+        maxTimeBetweenSpawns = GetNewMaxTimeBetweenSpawns();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeSinceLastSpawn >= maxTimeBetweenSpawns)
+        {
+            timeSinceLastSpawn = 0;
+            GameObject newBird = Instantiate(bird, transform.position, Quaternion.identity);
+            newBird.GetComponent<Bird>().isRight = isRight;
+        }
+        else
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+        }
+	}
+
+    private float GetNewMaxTimeBetweenSpawns()
+    {
+        return Random.Range(1, 3f);
+    }
+}
