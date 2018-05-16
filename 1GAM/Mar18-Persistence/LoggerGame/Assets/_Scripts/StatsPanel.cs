b7abc2f9062afa1cd9e0ea5ff754e9e8330e9980
Taylor <tbuchhei@gmail.com>
2018-05-15 22:04:05 -0700

    Drew bear and put him in the game

---
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/StatsPanel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using TMPro;
+
+public class StatsPanel : MonoBehaviour
+{
+
+    [SerializeField]
+    private static bool hide;
+    private float screenHeight;
+    private RectTransform rectTransform;
+    private EventManager eventManager;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        screenHeight = FindObjectOfType<Canvas>().pixelRect.height;
+        eventManager = FindObjectOfType<EventManager>();
+    }
+
+    private void Start()
+    {
+        hide = true;
+        DOTween.Init();
+    }
+
+    public void HideEventManagerPanel()
+    {
+        //crawl down
+        if (hide == true)
+        {
+            rectTransform.DOAnchorPosY(0 - (rectTransform.rect.height / 2), 1).OnComplete(() =>
+            {
+                if (eventManager.IsEventQueueEmpty() == false)
+                {
+                    eventManager.FadeBanner(0);
+                }
+
+
+            });
+            hide = false;
+        }
+        //crawl up
+        else
+        {
+            rectTransform.DOAnchorPosY(0 + (rectTransform.rect.height / 2), 1).OnComplete(() =>
+            {
+
+            });
+
+            hide = true;
+        }
+    }
+
+    public bool IsPanelHidden()
+    {
+        return hide;
+    }
+}
