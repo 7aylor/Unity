9881f6f458353ba67f65d9ec5be2bf6c474b5834
Taylor <tbuchhei@gmail.com>
2018-06-12 22:05:51 -0700

    Pixel Daily, Marble Run. Animated the house. Decided on Linked List in Building Manager

--- 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/BuildingManager.cs
+++ 1GAM/Mar18-Persistence/LoggerGame/Assets/_Scripts/BuildingManager.cs
@@ -6,7 +6,6 @@ public class BuildingManager : MonoBehaviour {
 
     public GameObject[] buildings;
     private int buildingCount;
-    private Vector2Int startPos; //holds the first building start position in array coords
     private Vector2Int lastBuildingPos; //holds the position of the last building spawned
     private enum lastNeighborChange { greater, lesser }
     lastNeighborChange lastNeighbor;
@@ -17,7 +16,7 @@ public class BuildingManager : MonoBehaviour {
     private Vector2Int greaterNeighbor; //indicate either neighbor above, or neighbor to the right
     private Vector2Int lesserNeighbor;  //indicate either neighbor below, or neighbor to the left
 
-    [SerializeField]private List<Vector2Int> buildingPositions; //all building positions on the map in array coords
+    private LinkedList<Vector2Int> buildingPositions;
 
     private void Awake()
     {
@@ -27,19 +26,14 @@ public class BuildingManager : MonoBehaviour {
     private void Start()
     {
         mapSides = new List<Vector2> { Vector2.left, Vector2.right, Vector2.up, Vector2.down };
-        buildingPositions = new List<Vector2Int>();
-        startPos = new Vector2Int(0, 0);
-        lastBuildingPos = startPos;
-        greaterNeighbor = startPos;
-        lesserNeighbor = startPos;
+        buildingPositions = new LinkedList<Vector2Int>();
+        lastBuildingPos = new Vector2Int(0, 0);
+        greaterNeighbor = lastBuildingPos;
+        lesserNeighbor = lastBuildingPos;
         buildingCount = 0;
 
         //temporarily call here
         FindSuitableSpawnPos();
-        //Invoke("FindSuitableSpawnPos", 1);
-        //Invoke("FindSuitableSpawnPos", 1);
-        //Invoke("FindSuitableSpawnPos", 1);
-        //Invoke("FindSuitableSpawnPos", 1);
     }
 
     /// <summary>
@@ -55,16 +49,14 @@ public class BuildingManager : MonoBehaviour {
     /// 
     /// </summary>
     private void FindSuitableSpawnPos()
-    {
-        startPos = lastBuildingPos;
-
+    { 
         //pick random edge then spawn first building in grass position
         if(buildingCount < 1)
         {
             do
             {
                 PickRandomSideTile();
-            } while (GameManager.instance.map[startPos.x, startPos.y] != (int)MapGenerator.tileType.grass);
+            } while (GameManager.instance.map[lastBuildingPos.x, lastBuildingPos.y] != (int)MapGenerator.tileType.grass);
 
             SpawnBuilding();
         }
@@ -162,7 +154,7 @@ public class BuildingManager : MonoBehaviour {
             }
             else if (side == Vector2.up || side == Vector2.down)
             {
-                startPos.x += 1;
+                lastBuildingPos.x += 1;
                 SpawnBuilding();
             }
         }
@@ -177,39 +169,36 @@ public class BuildingManager : MonoBehaviour {
 
         //update the map
         GameManager.instance.map[lastBuildingPos.x, lastBuildingPos.y] = (int)MapGenerator.tileType.building;
-        buildingPositions.Add(lastBuildingPos);
+        //buildingPositions.Add(lastBuildingPos);
 
         //spawn the house and update its parent
-        GameObject building = Instantiate(buildings[0].gameObject, new Vector3(GameManager.instance.ArrayCoordToWorldCoordX(startPos.x),
-            GameManager.instance.ArrayCoordToWorldCoordY(startPos.y), 0), Quaternion.identity);
+        GameObject building = Instantiate(buildings[0].gameObject, new Vector3(GameManager.instance.ArrayCoordToWorldCoordX(lastBuildingPos.x),
+            GameManager.instance.ArrayCoordToWorldCoordY(lastBuildingPos.y), 0), Quaternion.identity);
         building.transform.parent = buildingsParent;
 
         //delete the grass tile
         RaycastHit2D ray = Physics2D.Raycast(building.transform.position, Vector2.zero);
-
         if (ray.collider.gameObject.tag == "Grass")
         {
             Destroy(ray.collider.gameObject);
         }
 
         buildingCount++;
-        lastBuildingPos = startPos; //consider removing startPos since it is used as a temp variable
-        
 
         if(buildingCount > 1)
         {
             //update lastNeighbor to reflect new building placement
-            
+
             //increase
-            //if ()
-            //{
+            if (Random.Range(0, 2) == 1)
+            {
 
-            //}
+            }
             ////decrease
-            //else
-            //{
+            else
+            {
 
-            //}
+            }
         }
 
         SetNeighborCoords();
@@ -289,26 +278,26 @@ public class BuildingManager : MonoBehaviour {
         //pick left side of the map
         if (side == Vector2.left)
         {
-            startPos.x = 0;
-            startPos.y = Random.Range(0, GameManager.instance.sizeY);
+            lastBuildingPos.x = 0;
+            lastBuildingPos.y = Random.Range(0, GameManager.instance.sizeY);
         }
         //pick right side of the map
         else if (side == Vector2.right)
         {
-            startPos.x = GameManager.instance.sizeX - 1;
-            startPos.y = Random.Range(0, GameManager.instance.sizeY);
+            lastBuildingPos.x = GameManager.instance.sizeX - 1;
+            lastBuildingPos.y = Random.Range(0, GameManager.instance.sizeY);
         }
         //pick top of the map
         else if (side == Vector2.up)
         {
-            startPos.x = Random.Range(0, GameManager.instance.sizeX);
-            startPos.y = GameManager.instance.sizeY - 1;
+            lastBuildingPos.x = Random.Range(0, GameManager.instance.sizeX);
+            lastBuildingPos.y = GameManager.instance.sizeY - 1;
         }
         //pick bottom of the map
         else
         {
-            startPos.x = Random.Range(0, GameManager.instance.sizeX);
-            startPos.y = 0;
+            lastBuildingPos.x = Random.Range(0, GameManager.instance.sizeX);
+            lastBuildingPos.y = 0;
         }
     }
 }
